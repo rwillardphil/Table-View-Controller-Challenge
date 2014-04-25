761dd1d1bedea401457c733c8c9f75523c71b8ff
Ryan Willard <ryanwillard@rwillardphilMM.local>
2014-04-24 20:53:30 -0600

    Initial Commit

---
+++ RWTableViewController Challenge/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RWTableViewController Challenge
+//
+//  Created by Ryan Willard on 4/24/14.
+//  Copyright (c) 2014 Ryan Willard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RWAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RWAppDelegate class]));
+    }
+}
